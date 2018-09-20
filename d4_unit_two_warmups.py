7a7fb4cfbdfc5a3978cd28509262f38d49b95056
Min Woo <minwoo@iMac5.local>
2018-09-20 09:15:11 -0400

    Mathematical operators

---
+++ d4_unit_two_warmups.py
@@ -0,0 +1,22 @@
+print(9 * 5)
+
+print(2 / 5)
+
+# // sign is integer division
+
+print(12 // 5)
+
+print(27 // 4)
+
+print(2 // 5)
+
+# % sign gives the remainder
+# very useful when telling odd or even number
+
+print(5 % 2)
+
+print(9 % 5)
+
+print(6 % 6)
+
+print(2 % 7)
