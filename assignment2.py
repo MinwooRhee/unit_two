0b2f04003f6b71e0dbe36330297b6fe02f6629df
Min Woo <minwoo@iMac5.local>
2018-09-20 09:18:53 -0400

    program tells the angle between two lines

--- assignment2.py
+++ assignment2.py
@@ -8,10 +8,10 @@ import math
 
 import turtle
 
-print("This program will ask you to enter the coordinates of two points in a xy plain."
-      "Then it will draw two lines, one connecting the origin and the first point,"
-      "the other connecting the first point and the second point."
-      "It will also tell you the acute angle between the two lines in degrees.")
+print("This program will ask you to enter the coordinates of two points in a xy plain.")
+print("Then it will draw two lines, one connecting the origin and the first point,")
+print("the other connecting the first point and the second point.")
+print("It will also tell you the acute angle between the two lines in degrees.")
 
 x_coordinate_first_point = input("Input the x coordinate of the first point: ")
 
@@ -30,7 +30,7 @@ slope_second_line = (float(y_coordinate_second_point) - float(y_coordinate_first
 angle_in_radians = math.atan(abs(slope_first_line - slope_second_line)
                              / (1 + slope_first_line * slope_second_line))
 # math.atan() gives the arctangent of the value.
-# abs() gives the absolute value, which is useful when you have to subtract smaller variable from a bigger variable.
+# abs() gives the absolute value, which is useful when you have t*o subtract smaller variable from a bigger variable.
 
 angle_in_degrees = angle_in_radians * 180 / math.pi
 
